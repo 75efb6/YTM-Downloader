cac7e0e2f007b5c5742de862bb3fbe573ca64e6d
75efb6 <71222525+75efb6@users.noreply.github.com>
2024-08-25 20:02:20 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,39 @@
+import sys
+import re
+from time import sleep
+from yt_dlp import YoutubeDL as ytmd
+
+def main():
+    if len(sys.argv) < 2:
+        print('USAGE: main.py "<URL>" "<OUTPUT OPTIONS>"')
+        sys.exit(1)
+
+    url = sys.argv[1]
+    if not re.search("youtube.com", url):
+        print("Input is not a YT link.")
+        sys.exit(1)
+
+    out_opt = sys.argv[2] if len(sys.argv) > 2 else None
+
+    ydl_opts = {
+        "format": "mp3/bestaudio/best",
+        "postprocessors": [{
+            "key": "FFmpegExtractAudio",
+            "preferredcodec": "mp3",
+        }],
+    }
+
+    if out_opt:
+        print("Using custom output options...")
+        sleep(1)
+        ydl_opts["outtmpl"] = f"./out/{out_opt}"
+    else:
+        print("Using default output options...")
+        sleep(1)
+        ydl_opts["outtmpl"] = "./out/%(creator)s - %(title)s.%(ext)s"
+
+    with ytmd(ydl_opts) as ydl:
+        ydl.download([url])
+
+if __name__ == "__main__":
+    main()
